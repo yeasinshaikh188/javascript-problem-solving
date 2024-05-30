125aaee835543cafbd9c8ba0ce18891c17a71c5f
yeasinshaikh188@gmail.com <yeasinshaikh188@gmail.com>
2024-05-30 21:39:45 +0600

    creat a index.html and index.js file for js problem solving

---
+++ index.js
@@ -0,0 +1,43 @@
+// 1-------------->
+const carName = 'corolla';
+
+// 2 -------------->
+const firstName = "John";
+const lastName = "Doe";
+const age = 18;
+
+// 3--------------->
+let y = 10;
+let xx = 5 * y;
+
+// 4--------------->
+let length = 16;  //data type = number;
+let lastNames = "Johnson";  //data type = string;
+
+const x = {
+    firstName: "John",  
+    lastName: "Doe"
+  }; //data type objet;
+
+// 5----------------->
+function myFunction() {
+    alert("Hello World!");
+ }
+ myFunction();
+
+// 6---------------->
+let person = {
+    name: "John",
+    age : 50
+}
+person = alert('age: 50');
+
+// 7--------------->
+function clickTheButton (){
+    let showTheOutput = document.getElementById('text');
+    showTheOutput.innerHTML = 'Your Output :Welcome our normal page';
+}
+
+// 8-------------->
+
+
